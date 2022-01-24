6288c0409c6fae3a97d05d78f6ddd56563ddef97
saalahmadi <saalahmadi98@gmail.com>
2022-01-24 15:40:02 +0300

    add line chart

--- js/charts.js
+++ js/charts.js
@@ -1,7 +1,20 @@
 
 // Line Chart for Orders
 var ordersChart = document.getElementById("myAreaChart");
-
+const Line = new Chart(ordersChart, {
+    type: 'line',
+    data = {
+        labels: ['Aug1','Aug3','Aug5','Aug7','Aug9','Aug11','Aug13'],
+        datasets: [{
+          data: [10000, 20000, 30000, 40000],
+          borderColor: 'rgb(234, 68, 176)',
+          backgroundColor: 'rgb(200, 181, 221)'
+         
+        }]
+      }
+    
+});
+ 
 
 // Bar Chart for Visitor
 var visitorChart = document.getElementById("myBarChart");
